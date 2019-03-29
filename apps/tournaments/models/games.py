78f49e13f4de0090a52e7f98860c0d651735871b
Ivan López <ivan.lopez@studentpartner.com>
2019-03-29 03:31:36 -0600

    add bootstrap for mockup site

--- apps/tournaments/models/games.py
+++ apps/tournaments/models/games.py
@@ -1,10 +1,4 @@
 from django.db import models
 
 class Game(models.Model):
-    date = 
-    stadium =
-    referee_central =
-    referee_assist_one =
-    referee_assist_two =
-    score_final =
-    tournament =
+    pass
